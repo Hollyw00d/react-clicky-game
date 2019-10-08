59f5e55c9a04f753e5298e53fcac66455f052923
Matt Jennings <matt@mattjennings.net>
2019-10-08 14:04:40 -0700

    middle of creating clicky game

---
+++ src/card/index.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Card({ src, onClick }) {
+
+  return <img src={src} onClick={onClick} />
+
+}
---
+++ src/data/cards.js
@@ -0,0 +1,9 @@
+const cards = [
+  { image: '/img/1.png' },
+  { image: '/img/2.png' },
+  { image: '/img/3.png' },
+  { image: '/img/4.png' },
+
+]
+
+export default cards

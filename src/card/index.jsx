b99ab10f800443f8900de0bda8f39d897d7e8484
Matt Jennings <matt@mattjennings.net>
2019-10-08 20:44:35 -0700

    resized wrapper to fit images

--- src/card/index.jsx
+++ src/card/index.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const Card = ({ src, onClick, alt }) => <img src={src} onClick={onClick} alt={alt} />;
+const Card = ({ src, onClick, alt }) => <img src={src} onClick={onClick} alt={alt} width="200" height="199" />;
 
 export default Card;

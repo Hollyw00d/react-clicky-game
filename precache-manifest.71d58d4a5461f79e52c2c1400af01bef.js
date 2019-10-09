710ffd71b7acf1ed73bb44855692f0f493b2c462
Matt Jennings <matt@mattjennings.net>
2019-10-08 21:41:34 -0700

    Updates

---
+++ precache-manifest.71d58d4a5461f79e52c2c1400af01bef.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "338a3674996fac47d4a6ad9902e62c4a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f8606d5fa72eb6d83831",
+    "url": "/static/js/2.958f2b93.chunk.js"
+  },
+  {
+    "revision": "8289f5487696568c82db",
+    "url": "/static/js/main.7158a363.chunk.js"
+  },
+  {
+    "revision": "24a26739562a89a2d923",
+    "url": "/static/js/runtime-main.831fd3eb.js"
+  }
+]);

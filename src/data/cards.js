373b10bbaa02e725b601b9f2170e015d39e3f4fe
Matt Jennings <matt@mattjennings.net>
2019-10-08 22:07:38 -0700

    updated image path for react app

--- src/data/cards.js
+++ src/data/cards.js
@@ -1,38 +1,38 @@
 const cards = [
   { 
-    image: '/img/1.png', 
+    image: 'img/1.png', 
     alt: 'Morty'
   },
   { 
-    image: '/img/2.png', 
+    image: 'img/2.png', 
     alt: 'Mr. Meeseeks' 
   },
   { 
-    image: '/img/3.png', 
+    image: 'img/3.png', 
     alt: 'Rick Sanchez' 
   },
   { 
-    image: '/img/4.png', 
+    image: 'img/4.png', 
     alt: 'Beth Smith' 
   },
   { 
-    image: '/img/5.png', 
+    image: 'img/5.png', 
     alt: 'Birdperson' 
   },
   { 
-    image: '/img/6.png', 
+    image: 'img/6.png', 
     alt: 'Summer Smith 1' 
   },
   { 
-    image: '/img/7.png', 
+    image: 'img/7.png', 
     alt: 'Mr. Peanut' 
   },
   { 
-    image: '/img/8.png', 
+    image: 'img/8.png', 
     alt: 'Summer Smith 2' 
   },
   { 
-    image: '/img/9.png', 
+    image: 'img/9.png', 
     alt: 'Mr. Goldenfold' 
   },
 ];
